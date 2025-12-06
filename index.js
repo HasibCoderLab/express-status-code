ceff889cfb46a6bf2e2978c80fb1d3bf8be6fa2b
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-06 15:12:16 +0600

    feat status code

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from "express"
 const app = express();
 const port = 8000;
 let password = "123346"
+
 app.use(express.json());
+
 app.use((req,res,next) =>{
 if (req.body.pass != password ) {
-    res.send("Password does not match")
+     return  res.status(400).send("Password does not match")
 };
 next();
 
 });
 app.post( "/",(req,res) =>{
     console.log(req.body);
-    res.status(400).send({success:true})
+   return  res.status(200).send({success:true})
 })
 
 // app.get("/" , (req,res) =>{
